5e1a33f2c2d8b94e0d4b602195dc3e9b0a7947d4
Oscar Tree <oscarchan@greenaitech.com>
2025-08-05 18:41:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const venom = require('venom-bot');
+const express = require('express');
+const app = express();
+app.use(express.json());
 
+venom.create().then(client => {
+  app.post('/send', async (req, res) => {
+    const { groupId, message } = req.body;
+    try {
+      await client.sendText(groupId, message);
+      res.send({ status: 'sent' });
+    } catch (err) {
+      res.status(500).send({ error: err.message });
+    }
+  });
+
+  app.listen(3000, () => console.log('Venom API running on port 3000'));
+});
